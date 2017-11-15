ca970e3a3409765d9f442b397103b1e393e23803
Moa Lenngren <moa.mail@hotmail.com>
2017-11-15 15:07:37 +0100

    Initialize Project

--- BrewLikes_New2/app/src/main/java/moalenngren/brewlikes_new2/MainActivity.java
+++ BrewLikes_New2/app/src/main/java/moalenngren/brewlikes_new2/MainActivity.java
@@ -1,13 +1,13 @@
-package com.example.milja.brewlikes;
+package moalenngren.brewlikes_new2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AboutActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_main);
     }
 }
