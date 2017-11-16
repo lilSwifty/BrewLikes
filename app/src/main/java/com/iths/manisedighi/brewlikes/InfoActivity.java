dee25bbf85ea5a76b0eaebd0d6b7f2b84abf9ea6
emmapersson <emma.persson85@hotmail.com>
2017-11-17 00:01:13 +0100

    InfoActivity - InfoActivity with setupBottomNavigation method

--- app/src/main/java/com/iths/manisedighi/brewlikes/InfoActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/InfoActivity.java
@@ -1,16 +1,33 @@
 package com.iths.manisedighi.brewlikes;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
 /**
- * Emma
+ * Created by Emma on 2017-11-15.
  */
 public class InfoActivity extends AppCompatActivity {
+
+    private Context context = InfoActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        //for the bottom navigation
+        setupBottomNavigation();
+    }
+
+    /**
+     * A method that sets up the bottom navigation
+     */
+    public void setupBottomNavigation(){
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavigation);
+        BottomNavigationHelper.setupBottomNavigation(bottomNavigationViewEx);
+        BottomNavigationHelper.activateBottomNavigation(context, bottomNavigationViewEx);
     }
 }
