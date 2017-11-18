69fde7cf4d3d1125c792078ea408ba2d98d54302
victoriagronqvist <victoriagronqvist@gmail.com>
2017-11-18 12:41:44 +0100

    Declared the use of any camera - back, front or external in the manifest.

--- app/src/main/java/com/iths/manisedighi/brewlikes/CameraActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/CameraActivity.java
@@ -2,6 +2,7 @@ package com.iths.manisedighi.brewlikes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+//import android.hardware.camera2;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class CameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+    }
+
+
+
+    public void releaseCamera() {
+
     }
 }
