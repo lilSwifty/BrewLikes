19d572088460a71a1633c5a73f580d356654bb4f
KoolP <patrik.rikama@gmail.com>
2017-11-20 18:22:35 +0100

    Categories, added layout for bottom navigation, changed names of gategory to categories.

--- app/src/main/java/com/iths/manisedighi/brewlikes/CategoriesActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/CategoriesActivity.java
@@ -1,13 +1,28 @@
 package com.iths.manisedighi.brewlikes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class CategoriesActivity extends AppCompatActivity {
+    private Context context = CategoriesActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+        setupBottomNavigation();
     }
+
+    /**
+     * A method that sets up the bottom navigation
+     */
+    private void setupBottomNavigation(){
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavigation);
+        BottomNavigationHelper.manipulateBottomNavigation(bottomNavigationViewEx);
+        BottomNavigationHelper.activateBottomNavigation(context, bottomNavigationViewEx);
+    }
+
 }
