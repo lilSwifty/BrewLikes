7be225207d673eafa2ee4b48921a98055e115681
emmapersson <emma.persson85@hotmail.com>
2017-11-20 14:04:14 +0100

    Setup BottomNavigation in MainActivity

--- app/src/main/java/com/iths/manisedighi/brewlikes/MainActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/MainActivity.java
@@ -1,13 +1,28 @@
 package com.iths.manisedighi.brewlikes;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Context context = MainActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupBottomNavigation();
+    }
+
+    /**
+     * A method that sets up the bottom navigation
+     */
+    private void setupBottomNavigation(){
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavigation);
+        BottomNavigationHelper.manipulateBottomNavigation(bottomNavigationViewEx);
+        BottomNavigationHelper.activateBottomNavigation(context, bottomNavigationViewEx);
     }
 }
