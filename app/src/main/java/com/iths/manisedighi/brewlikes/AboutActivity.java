2aea391b0a823c8ef6fba4264b7ebe7aaa3465e4
Moa Lenngren <moa.mail@hotmail.com>
2017-11-20 16:19:56 +0100

    Upper toolbar with logo (launches MainActivity), camera (launches the camera) and about icon (launches the AboutActivity).

---
+++ app/src/main/java/com/iths/manisedighi/brewlikes/AboutActivity.java
@@ -0,0 +1,88 @@
+package com.iths.manisedighi.brewlikes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class AboutActivity extends AppCompatActivity {
+
+    ListView listView;
+    ImageView line;
+    ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = findViewById(R.id.toolbarTop);
+        setSupportActionBar(toolbar);
+        line = findViewById(R.id.lineImageView);
+        logo = findViewById(R.id.logoImageView);
+        listView = findViewById(R.id.membersListView);
+
+        /**
+         * When BrewLikes logo in the toolbar is clicked it launches MainActivity.
+         */
+        //TODO - Use flags here so the activities don't get put on the stack?
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent (AboutActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //ArrayList for all the members
+       ArrayList<String> memberList = new ArrayList<>();
+       memberList.add("EMMA");
+       memberList.add("MANI");
+       memberList.add("MILJA");
+       memberList.add("MOA");
+       memberList.add("PATRIK");
+       memberList.add("VICTOR");
+       memberList.add("VICTORIA");
+
+       //Array Adapter for the memberListView
+       ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, memberList);
+
+       //Connects the listView to the arrayAdapter
+       listView.setAdapter(arrayAdapter);
+    }
+
+    /**
+     * Upper toolbar with icons
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.camera_icon, menu);
+        return true;
+    }
+
+    /**
+     * Handles what happens when the icons in the toolbar are clicked
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.cameraIcon){
+            Intent intent = new Intent(this, CameraActivity.class);
+            startActivity(intent);
+            //TODO - Use flags here so the activities don't get put on the stack?
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
+
+
--- app/src/main/java/com/iths/manisedighi/brewlikes/MainActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/MainActivity.java
@@ -1,13 +1,62 @@
 package com.iths.manisedighi.brewlikes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbarTop);
+        setSupportActionBar(toolbar);
+        logo = findViewById(R.id.logoImageView);
+    }
+
+    /**
+     * Upper toolbar with icons
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.about_and_camera_icons, menu);
+        return true;
+    }
+
+    /**
+     * Handles what happens when the icons in the toolbar are clicked
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.aboutIcon){
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+
+        } else if(id == R.id.cameraIcon){
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
+
+
+
+
+
