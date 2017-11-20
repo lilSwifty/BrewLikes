578d0832e1b40af9a52c66e045ca8a15f63e61ee
Manolo <manisedighi@icloud.com>
2017-11-20 10:34:16 +0100

    Camera more updates, no manifest authorities

--- app/src/main/java/com/iths/manisedighi/brewlikes/CameraActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/CameraActivity.java
@@ -1,20 +1,43 @@
 package com.iths.manisedighi.brewlikes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 //import android.hardware.camera2;
 
 public class CameraActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CAPTURE = 1;
+    ImageView result_photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        Button click = (Button) findViewById(R.id.bCapture);
+        result_photo = (ImageView) findViewById(R.id.imageView);
     }
 
 
 
-    public void releaseCamera() {
+    public void launchCamera(View v) {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, REQUEST_CAPTURE);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap photo = (Bitmap) extras.get("data");
+            result_photo.setImageBitmap(photo);
+        }
     }
 }
