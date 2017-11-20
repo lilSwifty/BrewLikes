5117604ba3989c4aeb8ff416460d11cd2d321404
victoriagronqvist <victoriagronqvist@gmail.com>
2017-11-20 17:06:56 +0100

    Created the methods toast, dispatchTakePictureIntent, addPictureToGallery and scalePicture.
    The activity now saves the pictures to the already existing gallery on the camera.
    
    Had to comment out the code that contained mImageView since Mani got that part.

--- app/src/main/java/com/iths/manisedighi/brewlikes/CameraActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/CameraActivity.java
@@ -1,10 +1,25 @@
 package com.iths.manisedighi.brewlikes;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//import android.hardware.camera2;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraActivity extends AppCompatActivity {
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private static final String TAG = "CameraActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +28,101 @@ public class CameraActivity extends AppCompatActivity {
     }
 
 
+    String mCurrentPhotoPath;
 
-    public void releaseCamera() {
+    /**
+     * Creates a collision-resistant name for the image file
+     * @return the image with the new name
+     * @throws IOException - if
+     * TODO describe IOException, why using that?
+     */
+    private File createImageFile() throws IOException {
+        // Create a name for the image file
+        String dateStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + dateStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        //within parentheses: prefix, suffix, directory
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        //Save the file, path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    /**
+     * To create and invoke the Intent for the picture. First, ensure that there's a camera activity to handle the intent.
+     * TODO change comments, and comment the method.
+     */
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // To create a file to put the picture in
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ioException) {
+                //Error occured while creating the File
+                makeToast("Cannot create file");
+            }
+
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
 
     }
+
+    /**
+     * Adds the picture to the gallery
+     */
+    private void addPictureToGallery() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    /**
+     * TODO give mImageView the same name as the shown ImageView
+     * To scale down the picture and then decode it.
+     */
+    private void scalePicture() {
+        //The dimensions of the View
+        //int targetW = mImageView.getWidth();
+        //int targetH = mImageView.getHeight();
+
+        //The dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        //Decides how much to scale down the picture
+        //int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        //Decodes the image into Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        //bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        //mImageView.setImageBitmap(bitmap);
+
+
+    }
+
+
+    public void makeToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
---
+++ app/src/main/java/com/iths/manisedighi/brewlikes/SavePicturesFromCamera.java
@@ -0,0 +1,23 @@
+package com.iths.manisedighi.brewlikes;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
+
+/**
+ * Created by victoriagronqvist on 2017-11-18.
+ */
+
+public class SavePicturesFromCamera {
+
+}
