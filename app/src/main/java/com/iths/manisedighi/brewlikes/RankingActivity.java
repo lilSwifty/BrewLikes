4aab3debc2ce4a9253dbe037cb1c2ee8657d58bd
Fundberg <vfundberg@gmail.com>
2017-11-20 11:13:42 +0100

    uppdaterat rankingen

--- app/src/main/java/com/iths/manisedighi/brewlikes/RankingActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/RankingActivity.java
@@ -2,8 +2,20 @@ package com.iths.manisedighi.brewlikes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class RankingActivity extends AppCompatActivity {
+    private ImageView beerImage;
+    private TextView tasteText;
+    private RatingBar tasteRate;
+    private TextView priceText;
+    private RatingBar priceRate;
+    private EditText beerComment;
+    private Button rankingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
