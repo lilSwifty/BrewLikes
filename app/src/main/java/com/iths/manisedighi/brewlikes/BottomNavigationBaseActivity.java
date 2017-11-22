58cc001be48fb28f81af3ade2cc7d56085689998
emmapersson <emma.persson85@hotmail.com>
2017-11-22 21:49:56 +0100

    Cleaned up BottomNavigationBaseActivity

--- app/src/main/java/com/iths/manisedighi/brewlikes/BottomNavigationBaseActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/BottomNavigationBaseActivity.java
@@ -17,34 +17,34 @@ import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class BottomNavigationBaseActivity extends AppCompatActivity {
 
+    private BottomNavigationViewEx bottomNavigationViewEx;
+    private Context context = BottomNavigationBaseActivity.this;
+
     /**
      * A method that sets up the bottom navigation
      */
     protected void setupBottomNavigation(){
-        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavigation);
-        manipulateBottomNavigation(bottomNavigationViewEx);
-        activateBottomNavigation(this, bottomNavigationViewEx);
+        bottomNavigationViewEx = findViewById(R.id.bottomNavigation);
+        manipulateBottomNavigation();
+        activateBottomNavigation();
     }
 
     /**
-     * A static method that when called manipulates some of
+     * A method that when called manipulates some of
      * the settings for the bottom navigation
-     * @param view - the BottomNavigationViewEx to manipulate
      */
-    public void manipulateBottomNavigation(BottomNavigationViewEx view){
-        view.enableAnimation(false);
-        view.enableShiftingMode(false);
-        view.enableItemShiftingMode(false);
-        view.setTextVisibility(true);
+    public void manipulateBottomNavigation(){
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(true);
     }
 
     /**
-     * A static method that handles clicks on the different icons in the navigation view
-     * @param context - from where this method being called
-     * @param view - the BottomNavigationViewEx to handle
+     * A method that handles clicks on the different icons in the navigation view
      */
-    public void activateBottomNavigation(final Context context, BottomNavigationViewEx view){
-        view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private void activateBottomNavigation(){
+        bottomNavigationViewEx.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch(item.getItemId()){
