6322a1d3b4a48eccfb550253c1df7d8591d8ef0d
Milja <mmcvirtanen@gmail.com>
2017-11-22 15:15:36 +0100

    Method for returning all data in Category Table as arraylist.

--- app/src/main/java/com/iths/manisedighi/brewlikes/Category.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/Category.java
@@ -9,12 +9,30 @@ public class Category {
     private long id;
     private String name;
 
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Category() {
 
     }
 
-    public Category(String name) {
-        this.name = name;
-    }
+
 
 }
