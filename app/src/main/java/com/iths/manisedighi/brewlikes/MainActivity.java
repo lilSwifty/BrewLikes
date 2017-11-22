e81afc56e06e40213131b388351cbc55e24fab55
Moa Lenngren <moa.mail@hotmail.com>
2017-11-22 14:11:43 +0100

    isGpsServicesAvailable() that checks is user can use map. Some Google imports in gradle.

--- app/src/main/java/com/iths/manisedighi/brewlikes/MainActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/MainActivity.java
@@ -36,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
         private void init(){
 
         }
--- app/src/main/java/com/iths/manisedighi/brewlikes/MapActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/MapActivity.java
@@ -1,8 +1,21 @@
 package com.iths.manisedighi.brewlikes;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by moalenngren on 2017-11-22.
  */
 
-public class MapActivity {
+public class MapActivity extends AppCompatActivity {
+
+    private static final String TAG = "MapActivity";
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
 }
