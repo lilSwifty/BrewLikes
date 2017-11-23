79ed982039ec4afed435bc80d1bd96b2af1b106f
Moa Lenngren <moa.mail@hotmail.com>
2017-11-23 15:51:40 +0100

    Methods for getting the device's location, moving the camera to location and adding a dot on current location.

--- app/src/main/java/com/iths/manisedighi/brewlikes/MainActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
         private void init(){
-
+            //TODO - start map activity here with new Intent MapActivity.class
         }
 
         /**
