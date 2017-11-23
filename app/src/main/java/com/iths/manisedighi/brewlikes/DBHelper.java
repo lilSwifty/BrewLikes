9e3a16391d57ce28c4cf3560777de39c4122b31e
Milja <mmcvirtanen@gmail.com>
2017-11-23 18:51:51 +0100

    Added method getAllCategories()

--- app/src/main/java/com/iths/manisedighi/brewlikes/DBHelper.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/DBHelper.java
@@ -124,7 +124,7 @@ public class DBHelper extends SQLiteOpenHelper {
         //db.close();
     }
 
-    //ADD CATEGORY
+    //ADD CATEGORY - If user wants to add a category.
     public void addCategory(String categoryName) {
         Category category = new Category();
         Log.d("MyLog", "In addCategory method");
@@ -135,27 +135,23 @@ public class DBHelper extends SQLiteOpenHelper {
         values.put("COL_CATEGORY_NAME", categoryName);
         long id = db.insert(CATEGORY_TABLE, null, values);
         category.setId(id);
+        Log.d("MyLog", categoryName + " placed in row " + id);
         category.setName(categoryName);
-
-        //db.close();
     }
 
     /**
-     * Get the category name of the beer from the CATEGORY_TABLE
+     * Gets the category name of the beer from the Category Table and sets the value of beer.categoryName.
      * @param beer
-     * @return String
+     * @return String Name of the category
      */
     public String getBeerCategoryName(Beer beer) {
         Log.d("MyLog", "In getBeerCategoryName method");
         int id = beer.getCategoryId();
-        Log.d("MyLog", "Beer's category id is " + id);
 
         String selection = "_ID=?";
         String[] selectionArgs = new String[] { Integer.toString(id) };
         SQLiteDatabase db = getReadableDatabase();
         Cursor cursor = db.query(CATEGORY_TABLE, null, selection, selectionArgs, null, null, null, null);
-
-        Log.d("MyLog", "Done with cursor.");
 
         if (cursor.getCount() > 0) {
             cursor.moveToFirst();
@@ -284,14 +280,14 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     /**
-     * Returns all categories in CATEGORY_TABLE
-     * @return Arraylist
+     * Returns all category names in alphabetical order.
+     * @return Arraylist with category names in alphabetical order
      */
     public List<Category> getAllCategories() {
         List<Category> categoryList = new ArrayList<>();
         SQLiteDatabase db = getReadableDatabase();
 
-        Cursor cursor = db.query(CATEGORY_TABLE, null, null, null, null, null, "COL_CATEGORY_NAME ASC");
+        Cursor cursor = db.query(CATEGORY_TABLE, null, null, null, null, null, "COL_CATEGORY_NAME COLLATE NOCASE");
 
         boolean success = cursor.moveToFirst();
 
@@ -306,6 +302,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
             } while (cursor.moveToNext());
         }
+        cursor.close();
         return categoryList;
     }
 
