7245ee12db08a1850dc0bf368ef56269be489fff
Manolo <manisedighi@icloud.com>
2017-12-04 14:44:00 +0100

    Some comments added to RankingActivity

--- app/src/main/java/com/iths/manisedighi/brewlikes/RankingActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/RankingActivity.java
@@ -204,6 +204,12 @@ public class RankingActivity extends AppCompatActivity {
         }
          */
 
+        /*
+        else if(category.equals("Unknown")){
+            makeToast("Please choose category");
+        }
+         */
+
     }
 
     /**
