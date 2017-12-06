b17627c4085f910c432e3bef02b20230332d8530
victoriagronqvist <victoriagronqvist@gmail.com>
2017-12-06 14:03:06 +0100

    Fixed strings in strings.xml, also took away the info icon in top list activity

--- app/src/main/java/com/iths/manisedighi/brewlikes/TopListActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/TopListActivity.java
@@ -74,7 +74,7 @@ import android.widget.ListView;
      @Override
      public boolean onCreateOptionsMenu(Menu menu) {
          super.onCreateOptionsMenu(menu);
-         getMenuInflater().inflate(R.menu.about_and_camera_icons, menu);
+         getMenuInflater().inflate(R.menu.camera_icon, menu);
          return true;
      }
 
@@ -84,13 +84,7 @@ import android.widget.ListView;
      @Override
      public boolean onOptionsItemSelected(MenuItem item) {
          int id = item.getItemId();
-         if(id == R.id.aboutIcon){
-             Intent intent = new Intent(this, AboutActivity.class);
-             Log.d(TAG, "onOptionsItemSelected: aboutActivity clicked");
-             startActivity(intent);
-             return true;
-
-         } else if(id == R.id.cameraIcon){
+         if(id == R.id.cameraIcon){
              Intent cameraIntent = new Intent(this, RankingActivity.class);
              Log.d(TAG, "onOptionsItemSelected: RankingActivity clicked");
              startActivity(cameraIntent);
