8ac4e2e2837438363000575271fda9016045f45a
emmapersson <emma.persson85@hotmail.com>
2017-12-10 23:44:39 +0100

    A class to show a bigger beer picture

---
+++ app/src/main/java/com/iths/manisedighi/brewlikes/ShowBigBeerActivity.java
@@ -0,0 +1,43 @@
+package com.iths.manisedighi.brewlikes;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+
+/**
+ * Created by emmapersson on 2017-12-10.
+ */
+
+public class ShowBigBeerActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "BeerPhotoActivity";
+    private BitmapHelper bitmapHelper;
+    private String photoPath;
+    private Bitmap image;
+    private ImageView ivBigBeer;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: beerphoto");
+        setContentView(R.layout.activity_big_beer_picture);
+        showBigPicture();
+    }
+
+    private void showBigPicture() {
+        Intent intent = getIntent();
+        photoPath = intent.getStringExtra("photoPath");
+        ivBigBeer = findViewById(R.id.ivBigBeer);
+
+        image = BitmapFactory.decodeFile(photoPath);
+        ivBigBeer.setImageBitmap(image);
+
+        ivBigBeer.setAdjustViewBounds(true);
+        ivBigBeer.setImageBitmap(bitmapHelper.decodeSampledBitmapFromFile(photoPath, 960, 960));
+    }
+}
