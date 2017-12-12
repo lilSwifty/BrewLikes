70769f614fece491b587908a3204c82743cae648
Manolo <manisedighi@icloud.com>
2017-12-12 13:35:56 +0100

    photobug fixed, added link to youtubevideo in credits

--- app/src/main/java/com/iths/manisedighi/brewlikes/CreditActivity.java
+++ app/src/main/java/com/iths/manisedighi/brewlikes/CreditActivity.java
@@ -1,15 +1,34 @@
 package com.iths.manisedighi.brewlikes;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
 
 public class CreditActivity extends AppCompatActivity {
+
+    private WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credit);
     }
+
+    public void videoLink(View view){
+        String video_path = "https://www.youtube.com/watch?v=ttc_2GnCiT0&feature=youtu.be";
+        Uri uri = Uri.parse(video_path);
+
+// With this line the Youtube application, if installed, will launch immediately.
+// Without it you will be prompted with a list of the application to choose.
+        uri = Uri.parse("vnd.youtube:"  + uri.getQueryParameter("v"));
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+
 
 }
